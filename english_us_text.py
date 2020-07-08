201498f86d2e974b756db153a973a57918ab4bb7
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-07-08 14:45:03 -0400

    linting/ style improvements

--- english_us_text.py
+++ english_us_text.py
@@ -1,255 +1,169 @@
-menu : str
-menu = \
-  "What do you want to accomplish? \n\
-  \ 1) view the English phoneme inventory (as IPA graphemes). \n\
-  \ 2) make a phoneme voiced. \n\
-  \ 3) make a phoneme unvoiced. \n\
-  \ 4) describe a phoneme in English. \n\
-  \ 5) describe a phoneme in SPE Features. \n\
-  \ 6) divide IPA text into phonemes (chunk) \n\
-  \  \n\
-  \ Enter the number representing your selection below : str \n\
-  \ after the prompt : str and press enter/return. \n\
-  \ \n\
-  \ \n\
-  \ \n" 
+"""
+A module for containing any user facing text in the
+English language.
+"""
 
-userInput_viewEnglishPhonemeInventory : str
-userInput_makeAPhonemeVoiced : str
-userInput_makeAPhonemeUnvoiced : str
-userInput_describeAPhonemeInEnglish : str
-userInput_describeAPhonemeInSPE : str
-userInput_chunkIPAByPhoneme : str
-userInput_viewEnglishPhonemeInventory = "1"
-userInput_makeAPhonemeVoiced = "2"
-userInput_makeAPhonemeUnvoiced = "3"
-userInput_describeAPhonemeInEnglish = "4"
-userInput_describeAPhonemeInSPE = "5"
-userInput_chunkIPAByPhoneme = "6"
+menu: str = \
+  """What do you want to accomplish?
+1) view the English phoneme inventory (as IPA graphemes).
+2) make a phoneme voiced.
+3) make a phoneme unvoiced.
+4) describe a phoneme in English.
+5) describe a phoneme in SPE Features.
+6) divide IPA text into phonemes (chunk)
 
-prompt : str
-prompt = "(PROMPT:) "
+Enter the number representing your selection below:
+after the prompt: and press enter/return.\n\n\n"""
+
+
+print(menu)
+
+userInput_viewEnglishPhonemeInventory: str = "1"
+userInput_makeAPhonemeVoiced: str = "2"
+userInput_makeAPhonemeUnvoiced: str = "3"
+userInput_describeAPhonemeInEnglish: str = "4"
+userInput_describeAPhonemeInSPE: str = "5"
+userInput_chunkIPAByPhoneme: str = "6"
+
+prompt: str = "(PROMPT:) "
 
 
 
-sorryUnableToCalculate : str
-sorryUnableToCalculate = "Sorry : str unable to calculate answer with that input."
+sorryUnableToCalculate: str = "Sorry: str unable to calculate answer with that input."
 
-typeAPhoneme :  str
-typeAPhoneme = \
-  "Type a phoneme using IPA symbols : str and then press the enter key : str \
-  \ and the computer will display"
+typeAPhoneme: str = \
+"""Type a phoneme using IPA symbols: str and then press the enter key:
+ and the computer will display"""
 
 
-phonemeToDevoiceMessage : str
-phonemeToVoiceMessage : str
-phonemeToDescribeMessage : str
-phonemeToCalculateSPEMessage : str
-pleaseReadReadmeMessage : str
-programTerminatedNormallyMessage : str
-userSelectedMessage : str
-unrecognizedSelectionMessage : str
-noAnalysisFoundMessage : str 
-ipaTextToDivideMessage : str
-phonemeToDevoiceMessage = \
-  "Type a phoneme using IPA symbols : str and then press the enter key : str\
-  \ and the computer will display\
-  \ the devoiced counterpart (on the subsequent line):"
+phonemeToDevoiceMessage: str = \
+"""Type a phoneme using IPA symbols: str and then press the enter key:
+ and the computer will display
+ the devoiced counterpart (on the subsequent line):"""
 
-phonemeToVoiceMessage = \
-  "Type a phoneme using IPA symbols : str and then press the enter key : str\
-  \ and the computer will display\
-  \ the voiced counterpart (on the subsequent line):"
+phonemeToVoiceMessage: str = \
+"""Type a phoneme using IPA symbols: and then press the enter key:
+ and the computer will display
+ the voiced counterpart (on the subsequent line):"""
 
-phonemeToDescribeMessage = \
-  "Type a phoneme using IPA symbols : str and then press the enter key : str\
-  \ and the computer will display\
-  \ its English description (on the subsequent line):"
+phonemeToDescribeMessage: str = \
+"""
+Type a phoneme using IPA symbols: and then press the enter key:
+ and the computer will display
+ its English description (on the subsequent line):"""
 
-phonemeToCalculateSPEMessage = \
-  "Type a phoneme using IPA symbols : str and then press the enter key : str\
-  \ and the computer will display\
-  \ its SPE features (on the subsequent line):"
+phonemeToCalculateSPEMessage: str = \
+"""
+Type a phoneme using IPA symbols: and then press the enter key:
+ and the computer will display
+ its SPE features (on the subsequent line):"""
 
-ipaTextToDivideMessage = \
-  "Type text using IPA symbols : str and then press the enter key : str\
-  \ and the computer will display\
-  \ the text you entered with separate phonemes on separate lines:"
+ipaTextToDivideMessage: str = \
+"""
+Type text using IPA symbols: and then press the enter key:
+ and the computer will display
+ the text you entered with separate phonemes on separate lines:"""
 
-pleaseReadReadmeMessage = "Please read README.md file for instructions on how to use."
-programTerminatedNormallyMessage = "Program terminated normally."
-userSelectedMessage = "The user selected:"
-unrecognizedSelectionMessage =  "Unrecognized selection. No action taken."
-noAnalysisFoundMessage = "No analysis found!"
+pleaseReadReadmeMessage: str = "Please read README.md file for instructions on how to use."
+programTerminatedNormallyMessage: str = "Program terminated normally."
+userSelectedMessage: str = "The user selected:"
+unrecognizedSelectionMessage: str = "Unrecognized selection. No action taken."
+noAnalysisFoundMessage: str = "No analysis found!"
 
 
 
-noEnglishDescriptionFound_message : str
-noEnglishDescriptionFound_message = "(no English description found.)"
-
-
-consonant_Text : str
-vowel_Text : str
-front_BacknessText : str
-central_BacknessText : str
-back_BacknessText : str
-close_HeightText : str
-nearClose_HeightText : str
-closeMid_HeightText : str
-mid_HeightText : str
-openMid_HeightText : str
-nearOpen_HeightText : str
-open_HeightText : str
-rounded_RoundingText : str
-unrounded_RoundingText : str
-bilabial_PlaceText : str
-labioDental_PlaceText : str
-dental_PlaceText : str
-alveolar_PlaceText : str
-postAlveolar_PlaceText : str
-retroflex_PlaceText : str
-palatal_PlaceText : str
-velar_PlaceText : str
-uvular_PlaceText : str
-pharyngeal_PlaceText : str
-glottal_PlaceText : str
-epiglottal_PlaceText : str
-labialVelar_PlaceText : str
-labialPalatal_PlaceText : str
-alveoloPalatal_PlaceText : str
-palatoAlveolar_PlaceText : str
-plosive_MannerText : str
-nasal_MannerText : str
-trill_MannerText : str
-tapOrFlap_MannerText : str
-approximant_MannerText : str
-fricative_MannerText : str
-affricate_MannerText : str
-lateralFricative_MannerText : str
-lateralApproximant_MannerText : str
-lateralFlap_MannerText : str
-lateral_MannerText : str
-pulmonicEgressive_AirstreamText : str
-click_AirstreamText : str
-implosive_AirstreamText : str
-voiced_VocalFoldsText : str
-voiceless_VocalFoldsText : str
-voicedAspirated_VocalFoldsText : str
-voicelessAspirated_VocalFoldsText : str
-creakyVoiced_VocalFoldsText : str
-syllabic_PhonemeFeatureText : str
-consonantal_PhonemeFeatureText : str
-sonorant_PhonemeFeatureText : str
-continuant_PhonemeFeatureText : str
-voice_PhonemeFeatureText : str
-atr_PhonemeFeatureText : str
-nasal_PhonemeFeatureText : str
-lateral_PhonemeFeatureText : str
-delayedRelease_PhonemeFeatureText : str
-spreadGlottis_PhonemeFeatureText : str
-constrictedGlottis_PhonemeFeatureText : str
-labial_PhonemeFeatureText : str
-coronal_PhonemeFeatureText : str
-dorsal_PhonemeFeatureText : str
-pharyngeal_PhonemeFeatureText : str
-laryngeal_PhonemeFeatureText : str
-round_PhonemeFeatureText : str
-anterior_PhonemeFeatureText : str
-distributed_PhonemeFeatureText : str
-strident_PhonemeFeatureText : str
-high_PhonemeFeatureText : str
-low_PhonemeFeatureText : str
-back_PhonemeFeatureText : str
+noEnglishDescriptionFound_message: str = "(no English description found.)"
 
 
 
 
-consonant_Text                = "consonant"
-vowel_Text                    = "vowel"
 
-front_BacknessText            = "front"  
-central_BacknessText          = "central"
-back_BacknessText             = "back"   
+consonant_Text: str = "consonant"
+vowel_Text: str = "vowel"
 
-
-close_HeightText              = "close"     
-nearClose_HeightText          = "near-close"
-closeMid_HeightText           = "close-mid" 
-mid_HeightText                = "mid"       
-openMid_HeightText            = "open-mid"  
-nearOpen_HeightText           = "near-open" 
-open_HeightText               = "open"      
+front_BacknessText: str = "front"
+central_BacknessText: str = "central"
+back_BacknessText: str = "back"
 
 
-
-rounded_RoundingText          = "rounded"
-unrounded_RoundingText        = "unrounded"
-
-
-bilabial_PlaceText            = "bilabial"
-labioDental_PlaceText         = "labio-dental"
-dental_PlaceText              = "dental"
-alveolar_PlaceText            = "alveolar"
-postAlveolar_PlaceText        = "post-alveolar"
-retroflex_PlaceText           = "retroflex"
-palatal_PlaceText             = "palatal"
-velar_PlaceText               = "velar"
-uvular_PlaceText              = "uvular"
-pharyngeal_PlaceText          = "pharyngeal"
-glottal_PlaceText             = "glottal"
-epiglottal_PlaceText          = "epiglottal"
-labialVelar_PlaceText         = "labial-velar"
-labialPalatal_PlaceText       = "labial-palatal"
-alveoloPalatal_PlaceText      = "alveolo-palatal"
-palatoAlveolar_PlaceText      = "palato-alveolar"
-
-
-plosive_MannerText            = "plosive"
-nasal_MannerText              = "nasal"
-trill_MannerText              = "trill"
-tapOrFlap_MannerText          = "tap or flap"
-approximant_MannerText        = "approximant"
-fricative_MannerText          = "fricative"
-affricate_MannerText          = "affricate"
-lateralFricative_MannerText   = "lateral fricative"
-lateralApproximant_MannerText = "lateral approximant"
-lateralFlap_MannerText        = "lateral flap"
-lateral_MannerText            = "lateral"
+close_HeightText: str = "close"
+nearClose_HeightText: str = "near-close"
+closeMid_HeightText: str = "close-mid"
+mid_HeightText: str = "mid"
+openMid_HeightText: str = "open-mid"
+nearOpen_HeightText: str = "near-open"
+open_HeightText: str = "open"
 
 
 
-pulmonicEgressive_AirstreamText       = "pulmonic egressive"
-click_AirstreamText                   = "click"
-implosive_AirstreamText               = "implosive"
+rounded_RoundingText: str = "rounded"
+unrounded_RoundingText: str = "unrounded"
 
 
-voiced_VocalFoldsText                 = "voiced"
-voiceless_VocalFoldsText              = "voiceless"
-voicedAspirated_VocalFoldsText        = "voiced aspirated"
-voicelessAspirated_VocalFoldsText     = "voiceless aspirated"
-creakyVoiced_VocalFoldsText           = "creaky voiced"
+bilabial_PlaceText: str = "bilabial"
+labioDental_PlaceText: str = "labio-dental"
+dental_PlaceText: str = "dental"
+alveolar_PlaceText: str = "alveolar"
+postAlveolar_PlaceText: str = "post-alveolar"
+retroflex_PlaceText: str = "retroflex"
+palatal_PlaceText: str = "palatal"
+velar_PlaceText: str = "velar"
+uvular_PlaceText: str = "uvular"
+pharyngeal_PlaceText: str = "pharyngeal"
+glottal_PlaceText: str = "glottal"
+epiglottal_PlaceText: str = "epiglottal"
+labialVelar_PlaceText: str = "labial-velar"
+labialPalatal_PlaceText: str = "labial-palatal"
+alveoloPalatal_PlaceText: str = "alveolo-palatal"
+palatoAlveolar_PlaceText: str = "palato-alveolar"
 
 
-syllabic_PhonemeFeatureText           = "syllabic"
-consonantal_PhonemeFeatureText        = "consonantal"
-sonorant_PhonemeFeatureText           = "sonorant"
-continuant_PhonemeFeatureText         = "continuant"
-voice_PhonemeFeatureText              = "voice"
-atr_PhonemeFeatureText                = "ATR"
-nasal_PhonemeFeatureText              = "nasal"
-lateral_PhonemeFeatureText            = "lateral"
-delayedRelease_PhonemeFeatureText     = "delayed release"
-spreadGlottis_PhonemeFeatureText      = "spread glottis"
-constrictedGlottis_PhonemeFeatureText = "constricted glottis"
-labial_PhonemeFeatureText             = "labial"
-coronal_PhonemeFeatureText            = "coronal"
-dorsal_PhonemeFeatureText             = "dorsal"
-pharyngeal_PhonemeFeatureText         = "pharyngeal"
-laryngeal_PhonemeFeatureText          = "laryngeal"
-round_PhonemeFeatureText              = "round"
-anterior_PhonemeFeatureText           = "anterior"
-distributed_PhonemeFeatureText        = "distributed"
-strident_PhonemeFeatureText           = "strident"
-high_PhonemeFeatureText               = "high"
-low_PhonemeFeatureText                = "low"
-back_PhonemeFeatureText               = "back"
+plosive_MannerText: str = "plosive"
+nasal_MannerText: str = "nasal"
+trill_MannerText: str = "trill"
+tapOrFlap_MannerText: str = "tap or flap"
+approximant_MannerText: str = "approximant"
+fricative_MannerText: str = "fricative"
+affricate_MannerText: str = "affricate"
+lateralFricative_MannerText: str = "lateral fricative"
+lateralApproximant_MannerText: str = "lateral approximant"
+lateralFlap_MannerText: str = "lateral flap"
+lateral_MannerText: str = "lateral"
+
+
+
+pulmonicEgressive_AirstreamText: str = "pulmonic egressive"
+click_AirstreamText: str = "click"
+implosive_AirstreamText: str = "implosive"
+
+
+voiced_VocalFoldsText: str = "voiced"
+voiceless_VocalFoldsText: str = "voiceless"
+voicedAspirated_VocalFoldsText: str = "voiced aspirated"
+voicelessAspirated_VocalFoldsText: str = "voiceless aspirated"
+creakyVoiced_VocalFoldsText: str = "creaky voiced"
+
+
+syllabic_PhonemeFeatureText: str = "syllabic"
+consonantal_PhonemeFeatureText: str = "consonantal"
+sonorant_PhonemeFeatureText: str = "sonorant"
+continuant_PhonemeFeatureText: str = "continuant"
+voice_PhonemeFeatureText: str = "voice"
+atr_PhonemeFeatureText: str = "ATR"
+nasal_PhonemeFeatureText: str = "nasal"
+lateral_PhonemeFeatureText: str = "lateral"
+delayedRelease_PhonemeFeatureText: str = "delayed release"
+spreadGlottis_PhonemeFeatureText: str = "spread glottis"
+constrictedGlottis_PhonemeFeatureText: str = "constricted glottis"
+labial_PhonemeFeatureText: str = "labial"
+coronal_PhonemeFeatureText: str = "coronal"
+dorsal_PhonemeFeatureText: str = "dorsal"
+pharyngeal_PhonemeFeatureText: str = "pharyngeal"
+laryngeal_PhonemeFeatureText: str = "laryngeal"
+round_PhonemeFeatureText: str = "round"
+anterior_PhonemeFeatureText: str = "anterior"
+distributed_PhonemeFeatureText: str = "distributed"
+strident_PhonemeFeatureText: str = "strident"
+high_PhonemeFeatureText: str = "high"
+low_PhonemeFeatureText: str = "low"
+back_PhonemeFeatureText: str = "back"
