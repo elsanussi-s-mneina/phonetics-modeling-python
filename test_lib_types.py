849ec7ca3c98c032caf176603697a5d19f786213
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-07-10 12:38:41 -0400

    add unit test

--- test_lib_types.py
+++ test_lib_types.py
@@ -3,8 +3,11 @@ Unit tests for lib_types
 """
 
 import unittest
-from lib_types import Manner
+from lib_types import Manner, Airstream
 
 class TestLibTypes(unittest.TestCase):
     def test_plosive_and_lateral_approximant_are_different_manners_of_articulation(self):
         self.assertFalse(Manner.PLOSIVE == Manner.LATERAL_APPROXIMANT)
+
+    def test_pulmonic_and_click_are_different_airstream_mechanisms(self):
+        self.assertFalse(Airstream.PULMONIC_EGRESSIVE == Airstream.CLICK)
