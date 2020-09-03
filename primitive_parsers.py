8f5d51c563b1bdc806337c88dd524f59fae29c05
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-09-03 16:25:47 -0500

    minor: whitespace

--- primitive_parsers.py
+++ primitive_parsers.py
@@ -14,7 +14,11 @@ def single_char_parser(char_list: List[str], text: str) -> Optional[Tuple[str, s
         return None
 
 
-def then_parser(first_parser: Callable[[str], Optional[Tuple[str, str]]], second_parser: Callable[[str], Optional[Tuple[str, str]]], text: str) -> Optional[Tuple[str, str]]:
+def then_parser(
+    first_parser: Callable[[str], Optional[Tuple[str, str]]],
+    second_parser: Callable[[str], Optional[Tuple[str, str]]],
+    text: str,
+) -> Optional[Tuple[str, str]]:
     """
     Uses one parser on the text,
     then uses the next parser on the remaining
@@ -22,30 +26,36 @@ def then_parser(first_parser: Callable[[str], Optional[Tuple[str, str]]], second
     """
     first_result = first_parser(text)
     if first_result is None:
-       return None
+        return None
     else:
         parsed, rest = first_result
         second_result = second_parser(rest)
         if second_result is None:
-           return None
+            return None
         else:
-           parsed_2, rest_2 = second_result
-           return parsed + parsed_2, rest_2
+            parsed_2, rest_2 = second_result
+            return parsed + parsed_2, rest_2
 
 
-def or_parser(first_parser: Callable[[str], Optional[Tuple[str, str]]], second_parser: Callable[[str], Optional[Tuple[str, str]]], text: str) -> Optional[Tuple[str, str]]:
-  """
-  combines parsers by using one or the other.
-  """
-  result = first_parser(text)
-  if result is None:
-    result_2 = second_parser(text)
-    return result_2
-  else:
-    return result
+def or_parser(
+    first_parser: Callable[[str], Optional[Tuple[str, str]]],
+    second_parser: Callable[[str], Optional[Tuple[str, str]]],
+    text: str,
+) -> Optional[Tuple[str, str]]:
+    """
+    combines parsers by using one or the other.
+    """
+    result = first_parser(text)
+    if result is None:
+        result_2 = second_parser(text)
+        return result_2
+    else:
+        return result
 
 
-def many_parser(sub_parser: Callable[[str], Optional[Tuple[str, str]]], text: str) -> Optional[Tuple[str, str]]:
+def many_parser(
+    sub_parser: Callable[[str], Optional[Tuple[str, str]]], text: str
+) -> Optional[Tuple[str, str]]:
     """
     changes a parser by repeating it an indefinite number
     of times.
@@ -57,23 +67,24 @@ def many_parser(sub_parser: Callable[[str], Optional[Tuple[str, str]]], text: st
     if result is None:
         return None
     else:
-      parsed, rest = result
-      result_2 = many_parser(sub_parser, rest) 
-      if result_2 is None:
-          return parsed, rest
-      else:
-          parsed_2, rest_2 = result_2
-          return (parsed + parsed_2, rest_2)
+        parsed, rest = result
+        result_2 = many_parser(sub_parser, rest)
+        if result_2 is None:
+            return parsed, rest
+        else:
+            parsed_2, rest_2 = result_2
+            return (parsed + parsed_2, rest_2)
 
 
-
-def optional_parser(sub_parser: Callable[[str], Optional[Tuple[str, str]]], text: str) -> Optional[Tuple[str, str]]:
-  """
-  changes a parser by making it never return Nothing,
-  that is it makes the parser optional.
-  """
-  result = sub_parser(text)
-  if result is None:
-    return ("", text) # return text unconsumed
-  else:
-    return result
+def optional_parser(
+    sub_parser: Callable[[str], Optional[Tuple[str, str]]], text: str
+) -> Optional[Tuple[str, str]]:
+    """
+    changes a parser by making it never return Nothing,
+    that is it makes the parser optional.
+    """
+    result = sub_parser(text)
+    if result is None:
+        return ("", text)  # return text unconsumed
+    else:
+        return result
--- test_primitive_parsers.py
+++ test_primitive_parsers.py
@@ -3,131 +3,229 @@ unit tests for primitive parsers
 """
 
 import unittest
-from primitive_parsers import single_char_parser, then_parser, many_parser, optional_parser, or_parser
 from functools import partial
+from primitive_parsers import (
+    single_char_parser,
+    then_parser,
+    many_parser,
+    optional_parser,
+    or_parser,
+)
+
 
 class TestPrimitiveParsers(unittest.TestCase):
     def test_single_char_parser_empty_text(self) -> None:
         """single character parser"""
         expected = None
         actual = single_char_parser([], "")
-        self.assertEqual(expected, actual,
-            msg="should be that: single character parser of no characters fails to parse empty text"
-            )
+        self.assertEqual(
+            expected,
+            actual,
+            msg="should be that: single character parser "
+            + "of no characters fails to parse empty text",
+        )
 
     def test_single_char_parser_empty_parser_and_text_of_length_3(self) -> None:
         expected = None
         actual = single_char_parser([], "abc")
-        self.assertEqual(expected, actual,
-            msg="should be that: single character parser of no characters fails to parse text of length 3")
+        self.assertEqual(
+            expected,
+            actual,
+            msg="should be that: single character parser of "
+            + "no characters fails to parse text of length 3",
+        )
 
-    def test_single_char_parser_parser_and_text_of_length_parses_nothing(self) -> None:
-        self.assertEqual(None, single_char_parser(["a"], "b"),
-                         msg="should be that: single character parser of the character 'a' fails to parse the character \"b\"")
+    def test_single_char_parser_parser_and_text_of_length_parses_nothing(
+        self,
+    ) -> None:
+        self.assertEqual(
+            None,
+            single_char_parser(["a"], "b"),
+            msg="should be that: single character parser of"
+            + " the character 'a' fails to parse the character \"b\"",
+        )
 
-    def test_single_char_parser_parser_and_text_of_length_parses_one_character(self) -> None:
-        self.assertEqual(("a", ""), single_char_parser(["a"], "a"),
-                         msg="should be that: single character parser of the character 'a' does parse the character \"a\"")
+    def test_single_char_parser_parser_and_text_of_length_parses_one_character(
+        self,
+    ) -> None:
+        self.assertEqual(
+            ("a", ""),
+            single_char_parser(["a"], "a"),
+            msg="should be that: single character parser of the character 'a' does parse the character \"a\"",
+        )
 
-    def test_single_char_parser_parser_and_text_of_length_parses_one_character_leaves_one(self) -> None:
-        self.assertEqual(("a", "a"), single_char_parser(['a'], "aa"),
-                         msg="should be that: single character parser of the character 'a' does parse the string containing two \"aa\" characters and leaves one left")
+    def test_single_char_parser_parser_and_text_of_length_parses_one_character_leaves_one(
+        self,
+    ) -> None:
+        self.assertEqual(
+            ("a", "a"),
+            single_char_parser(["a"], "aa"),
+            msg="should be that: single character parser of the character 'a' does parse "
+            + 'the string containing two "aa" characters and leaves one left',
+        )
 
     def test_single_char_parser_2(self) -> None:
-        self.assertEqual(single_char_parser(['a', 'b'], "ab"), ("a", "b"),
-            msg="should be that: single character parser of the character 'a' does parse the string containing \"ab\"\
-             \ characters and leaves \"b\"")
+        self.assertEqual(
+            single_char_parser(["a", "b"], "ab"),
+            ("a", "b"),
+            msg="should be that: single character parser of the character 'a' "
+            + "does parse the string containing 'ab'"
+            + " characters and leaves 'b'",
+        )
 
     def test_single_char_parser_3(self) -> None:
-        self.assertEqual(single_char_parser(['a', 'b'],"abc"), ("a", "bc"),
-            msg=  "should be that: single character parser of the character 'a' does parse the string containing \"abc\"\
-            \ characters and leaves \"bc\"")
+        self.assertEqual(
+            single_char_parser(["a", "b"], "abc"),
+            ("a", "bc"),
+            msg="should be that: single character parser of the character 'a' "
+            + "does parse the string containing 'abc'"
+            " characters and leaves 'bc'",
+        )
 
     def test_single_char_parser_4(self) -> None:
-        self.assertEqual(single_char_parser(['a', 'b'], "cba"), None,
-            msg="should be that: single character parser of the character 'a' does parse the string containing \"abc\"\
-            \ characters and leaves \"bc\"")
+        self.assertEqual(
+            single_char_parser(["a", "b"], "cba"),
+            None,
+            msg="should be that: single character parser of "
+            + "the character 'a' does parse the string containing 'abc'"
+            + "characters and leaves 'bc' ",
+        )
 
     def test_then_parser(self) -> None:
-         """then-parser"""
-         self.assertEqual(then_parser(partial(single_char_parser, ['a']), partial(single_char_parser, ['b']), "abc"), ("ab", "c"),
-            msg="should be that: combining two single character parsers, parses two characters in same order")
+        """then-parser"""
+        self.assertEqual(
+            then_parser(
+                partial(single_char_parser, ["a"]),
+                partial(single_char_parser, ["b"]),
+                "abc",
+            ),
+            ("ab", "c"),
+            msg="should be that: combining two single character parsers,"
+            + " parses two characters in same order",
+        )
 
     def test_then_parser_2(self) -> None:
-        self.assertEqual(then_parser(partial(single_char_parser, ['a']), partial(single_char_parser, ['b']), "abc"),
-                                     ("ab", "c"),
-           msg="should be that: combining two single character parsers, does not parse two characters in opposite order")
+        self.assertEqual(
+            then_parser(
+                partial(single_char_parser, ["a"]),
+                partial(single_char_parser, ["b"]),
+                "abc",
+            ),
+            ("ab", "c"),
+            msg="should be that: combining two single character parsers,"
+            + " does not parse two characters in opposite order",
+        )
 
     def test_then_parser_3(self) -> None:
-        self.assertEqual(then_parser(partial(single_char_parser, ['a']), partial(single_char_parser, ['b']), "bac"),
-        None,
-        msg="should be that: combining two single character parsers, parses two characters")
+        self.assertEqual(
+            then_parser(
+                partial(single_char_parser, ["a"]),
+                partial(single_char_parser, ["b"]),
+                "bac",
+            ),
+            None,
+            msg="should be that: combining two single character parsers, parses two characters",
+        )
 
     def test_many_parser(self) -> None:
-         """should be that: a many-parser on one character succeeds parsing when the character is the same."""
-         self.assertEqual(many_parser(partial(single_char_parser, ['a']), "a"), ("a", ""))
-         self.assertEqual(many_parser(partial(single_char_parser, ['b']), "b"), ("b", ""))
-         self.assertEqual(many_parser(partial(single_char_parser, ['3']), "3"), ("3", ""))
+        """
+        should be that: a many-parser on one character
+        succeeds parsing when the character is the same.
+        """
+        self.assertEqual(
+            many_parser(partial(single_char_parser, ["a"]), "a"), ("a", "")
+        )
+        self.assertEqual(
+            many_parser(partial(single_char_parser, ["b"]), "b"), ("b", "")
+        )
+        self.assertEqual(
+            many_parser(partial(single_char_parser, ["3"]), "3"), ("3", "")
+        )
 
     def test_many_parser_1(self) -> None:
-         """should be that: a many-parser on one character fails when parsing a string that does not start with that character."""
-         self.assertEqual(many_parser(partial(single_char_parser, ['a']), "baa"), None)
-         self.assertEqual(many_parser(partial(single_char_parser, ['z']), "az"), None)
+        """should be that: a many-parser on one character fails
+        when parsing a string that does not start with that character."""
+        self.assertEqual(many_parser(partial(single_char_parser, ["a"]), "baa"), None)
+        self.assertEqual(many_parser(partial(single_char_parser, ["z"]), "az"), None)
 
     def test_many_parser_2(self) -> None:
-        """should be that: a many-parser on one characters succeeds on a string that contains only that character"""
-        self.assertEqual(many_parser(partial(single_char_parser, ['f']), "fff"), ("fff", ""))
-        self.assertEqual(many_parser(partial(single_char_parser, ['f']), "fffa"), ("fff", "a"))
-        self.assertEqual(many_parser(partial(single_char_parser, ['d']), "ddrst"), ("dd", "rst"))
-    
-    def test_or_parser_3(self) -> None:
-         """or-parser"""
-         # it parses "aaaf" successfully
-         expected = ("aaa", "f")
-         actual = or_parser(partial(many_parser, partial(single_char_parser, ['f'])), \
-                            partial(many_parser, partial(single_char_parser, ['a'])), "aaaf")
-         self.assertEqual(actual, expected)
+        """should be that: a many-parser on one characters
+        succeeds on a string that contains only that character"""
+        self.assertEqual(
+            many_parser(partial(single_char_parser, ["f"]), "fff"), ("fff", "")
+        )
+        self.assertEqual(
+            many_parser(partial(single_char_parser, ["f"]), "fffa"),
+            ("fff", "a"),
+        )
+        self.assertEqual(
+            many_parser(partial(single_char_parser, ["d"]), "ddrst"),
+            ("dd", "rst"),
+        )
 
+    def test_or_parser_3(self) -> None:
+        """or-parser"""
+        # it parses "aaaf" successfully
+        expected = ("aaa", "f")
+        actual = or_parser(
+            partial(many_parser, partial(single_char_parser, ["f"])),
+            partial(many_parser, partial(single_char_parser, ["a"])),
+            "aaaf",
+        )
+        self.assertEqual(actual, expected)
 
     def test_or_parser_4(self) -> None:
-         """or-parser"""
-         # it parses "ffffa" successfully
-         expected = ("f", "fffa")
-         actual = or_parser(partial(single_char_parser, ['f']), \
-                            partial(many_parser, partial(single_char_parser, ['a'])), "ffffa")
-         self.assertEqual(actual, expected)
-
+        """or-parser"""
+        # it parses "ffffa" successfully
+        expected = ("f", "fffa")
+        actual = or_parser(
+            partial(single_char_parser, ["f"]),
+            partial(many_parser, partial(single_char_parser, ["a"])),
+            "ffffa",
+        )
+        self.assertEqual(actual, expected)
 
     def test_or_parser_5(self) -> None:
-         """or-parser"""
-         # parse failure case
-         expected = None
-         actual = or_parser(partial(many_parser, partial(single_char_parser, ['a', 'b'])), \
-                            partial(many_parser, partial(single_char_parser, ['k'])), "ttttnnn")
-         self.assertEqual(actual, expected)
+        """or-parser"""
+        # parse failure case
+        expected = None
+        actual = or_parser(
+            partial(many_parser, partial(single_char_parser, ["a", "b"])),
+            partial(many_parser, partial(single_char_parser, ["k"])),
+            "ttttnnn",
+        )
+        self.assertEqual(actual, expected)
 
     def test_optional_parser(self) -> None:
         """optional parser: parses "aaaf" successfully"""
         expected = ("aaa", "f")
-        actual = optional_parser(partial(many_parser, partial(single_char_parser, ['a'])), "aaaf")
+        actual = optional_parser(
+            partial(many_parser, partial(single_char_parser, ["a"])), "aaaf"
+        )
         self.assertEqual(actual, expected)
 
     def test_optional_parser_succeeds_not_consuming_input(self) -> None:
         """it "parses "bbb" successfully without consuming any input"""
         expected = ("", "bbb")
-        actual = optional_parser(partial(many_parser, partial(single_char_parser, ['a'])), "bbb")
+        actual = optional_parser(
+            partial(many_parser, partial(single_char_parser, ["a"])), "bbb"
+        )
         self.assertEqual(actual, expected)
 
-    def test_optional_parser_succeeds_consuming_input(self) -> None:
-        """it "parses "bbb" successfully without consuming any input"""
+    def test_optional_parser_succeeds_consuming_input_f(self) -> None:
+        """it "parses "f" successfully without consuming any input"""
         expected = ("f", "")
-        actual = optional_parser(partial(many_parser, partial(single_char_parser, ['f'])), "f")
+        actual = optional_parser(
+            partial(many_parser, partial(single_char_parser, ["f"])), "f"
+        )
         self.assertEqual(actual, expected)
 
-    def test_optional_parser_succeeds_not_consuming_input(self) -> None:
+    def test_optional_parser_succeeds_not_consuming_input_f(self) -> None:
         """
         does parse "d" when expecting an "f", but does not consume "d"
         """
         expected = ("", "d")
-        actual = optional_parser(partial(many_parser, partial(single_char_parser, ['f'])), "d")
+        actual = optional_parser(
+            partial(many_parser, partial(single_char_parser, ["f"])), "d"
+        )
         self.assertEqual(actual, expected)
